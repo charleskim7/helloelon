057025a3ad73a0e582fd5bfdd8f7209468802ce0
Charles Kim <charleskim@charless-air.elon.edu>
2016-10-13 18:19:23 -0400

    whole file

---
+++ Junior/4. Computer Graphics & Game Programming/workspace/Maze/Assets/FPSInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSInput : MonoBehaviour {
+	public float speed = 6.0f;
+	public float gravity = -9.8f;
+	public float jumpSpeed = 100f;
+	public float vertSpeed = 0f;
+
+	private CharacterController charController;
+
+	void Start() {
+		charController = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+		// Move
+		float deltaX = Input.GetAxis("Horizontal") * speed;
+		float deltaZ = Input.GetAxis("Vertical") * speed;
+		Vector3 movement = new Vector3(deltaX, vertSpeed, deltaZ);
+		movement = Vector3.ClampMagnitude(movement, speed);
+
+		// movement.y = gravity;
+
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection(movement);
+		charController.Move(movement);
+
+		// Jump
+		if (charController.isGrounded) {
+			if (Input.GetButtonDown ("Jump")) {
+				vertSpeed = jumpSpeed;
+			} else {
+				vertSpeed = 0;
+			}
+		} else {
+			vertSpeed += gravity;
+		}
+		movement = new Vector3(deltaX, vertSpeed, deltaZ);
+	}
+}
---
+++ Junior/4. Computer Graphics & Game Programming/workspace/Maze/Assets/RayShooter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+
+	private Camera camera;
+
+	// Locks to camera view
+	void Start () {
+		camera = GetComponent<Camera> ();
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Shoots bullet upon click
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 origin = new Vector3 (camera.pixelWidth / 2, camera.pixelHeight / 2, 0);
+			Ray ray = camera.ScreenPointToRay (origin);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				StartCoroutine (Shoot (hit.point));    
+			}
+		}
+	}
+
+	// Bullets disappear after 1 second
+	private IEnumerator Shoot(Vector3 position){
+		GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphere.transform.position = position;
+		sphere.transform.localScale = new Vector3(0.25f,0.25f,0.25f);
+		yield return new WaitForSeconds (1);
+		Destroy (sphere);
+	}
+
+//	Ray ray = new Ray(transform.position, transform.forward);
+//	RaycastHit hit;
+//	if (Physics.RayCast(ray, out hit)){
+//		GameObject target = hit.transform.gameObject;
+//	}
+}
